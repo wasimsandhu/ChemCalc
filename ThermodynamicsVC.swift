9b619b8e40da11ded92b4d1cf2393c55967ad6f8
Wasim Sandhu <wasimsandhu@users.noreply.github.com>
2017-12-31 20:08:48 -0800

    Thermodynamics tabs now functional
    The table view switches between retrieved values of enthalpy, entropy and spontaneity successfully now

--- ThermodynamicsVC.swift
+++ ThermodynamicsVC.swift
@@ -14,25 +14,36 @@ class ThermodynamicsVC: UIViewController, UITableViewDataSource, UITableViewDele
     @IBOutlet weak var thermoTableView: UITableView!
     @IBOutlet weak var tabs: UISegmentedControl!
     var compoundData = [ThermoDataObject]()
+    var tag = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Firebase database reference
         let rootRef = FIRDatabase.database().reference()
-        
         rootRef.child("Thermodynamics").observe(.childAdded, with: { (snapshot) in
-            
             if let dictionary = snapshot.value as? [String:AnyObject] {
                 let compound = ThermoDataObject(dictionary: dictionary)
                 self.compoundData.append(compound)
             }
-            
             DispatchQueue.main.async(execute: {
                 self.thermoTableView.reloadData()
             })
-            
         })
+        
+        tabs.addTarget(self, action: #selector(segmentedControlValueChanged), for: .valueChanged)
+    }
+    
+    @objc func segmentedControlValueChanged(segment: UISegmentedControl) {
+        if tabs.selectedSegmentIndex == 0 {
+            tag = 0
+        } else if tabs.selectedSegmentIndex == 1 {
+            tag = 1
+        } else if tabs.selectedSegmentIndex == 2 {
+            tag = 2
+        }
+        
+        thermoTableView.reloadData()
     }
     
     /* Table View */
@@ -43,9 +54,19 @@ class ThermodynamicsVC: UIViewController, UITableViewDataSource, UITableViewDele
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = thermoTableView.dequeueReusableCell(withIdentifier: "tcell", for: indexPath) as? ThermoCell
         
-        let compound = compoundData[indexPath.row]
-        cell?.compoundLabel.text = compound.name
-        cell?.deltaValueLabel.text = compound.enthalpy! + " kJ/mol"
+        if (tag == 0) {
+            let compound = compoundData[indexPath.row]
+            cell?.compoundLabel.text = compound.name
+            cell?.deltaValueLabel.text = compound.enthalpy! + " kJ/mol"
+        } else if (tag == 1) {
+            let compound = compoundData[indexPath.row]
+            cell?.compoundLabel.text = compound.name
+            cell?.deltaValueLabel.text = compound.entropy! + " kJ/mol"
+        } else if (tag == 2) {
+            let compound = compoundData[indexPath.row]
+            cell?.compoundLabel.text = compound.name
+            cell?.deltaValueLabel.text = compound.spontaneity! + " J/mol•K"
+        }
         
         return cell!
     }
